522bdbd9def55112cd9ae5cf14990314e70a0ea4
soohae207 <soohae207@gmail.com>
2026-08-16 15:21:43 +0530

    Create variables.py

---
+++ input_output.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+
+print("Hello", name)
+print("You are", age, "years old")
---
+++ variables.py
@@ -0,0 +1,8 @@
+name = "Suhani"
+age = 18
+height = 1.52
+student = True 
+print("Hello", name)
+print("Your age is", age)
+print("Your height is", height)
+print("Are you student?", student)
